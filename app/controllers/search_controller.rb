bb2a6adcba098139ba4f13f0967fb8655a195ac6
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-29 10:33:28 -0600

    Update search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,8 @@ class SearchController < ApplicationController
   def index
     @searchresults = SearchFacade.new(search_params)
   end
-
+private
+  def search_params
+    params.permit(:title)
+  end
 end
