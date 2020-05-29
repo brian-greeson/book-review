e9d952a05bb3eb9a5fdd9c6bfcac82cc67b69948
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-29 10:35:48 -0600

    Update search facade with serializer calls

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -2,7 +2,8 @@ class SearchFacade < BaseFacade
   def initialize(search_params)
     book_json = OpenLibraryService.new.search_by_title(search_params[:title])
     review_json = NytBookService.new.search_by_title(search_params[:title])
-    @book = Book.new(title: searchparams[:title],)
+    @book = Book.injest(book_json)
+    @reviews = Reviews.injest(review_json)
   end
 
 end
