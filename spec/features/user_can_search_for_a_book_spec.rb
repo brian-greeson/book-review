41fa648807490654eb09a34c66360d711146ad6a
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-29 09:59:10 -0600

    Remove find_css from spec

--- spec/features/user_can_search_for_a_book_spec.rb
+++ spec/features/user_can_search_for_a_book_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe 'As a user' do
       fill_in('title', with: 'Normal People')
       click_on 'commit'
       expect(current_path).to eq("/search")
-      within(find_css(".book_info")) do
-        expect(find_css(".book_title")).to have_content("Normal People")
-        expect(find_css(".book_author")).to have_content("Sally Rooney")
-        expect(find_css(".book_publisher")).to have_content("Faber & Faber, Hogarth, Random House Large Print, Knopf Canada")
+      within(".book_info") do
+        expect(".book_title").to have_content("Normal People")
+        expect(".book_author").to have_content("Sally Rooney")
+        expect(".book_publisher").to have_content("Faber & Faber, Hogarth, Random House Large Print, Knopf Canada")
       end
-      within(find_css(".reviews")) do
-        expect(find_css(".review_count")).to have_content("2")
-        expect(find_css(".review").count).to eq 2
-        expect(find_css(".review").first).to include("Rooney's second novel tracks two gifted but troubled teenagers across four years of friendship and occasional romance.")
+      within(".reviews") do
+        expect(".review_count").to have_content("2")
+        expect(".review".count).to eq 2
+        expect(".review".first).to include("Rooney's second novel tracks two gifted but troubled teenagers across four years of friendship and occasional romance.")
         expect(page).to have_content("Review Publication Date: 2019-04-08")
-        expect(find_css(".review").second).to include("Like \"Conversations With Friends,\" \"Normal People\" also traces a young romance in Ireland.")
+        expect(".review".second).to include("Like \"Conversations With Friends,\" \"Normal People\" also traces a young romance in Ireland.")
         expect(page).to have_content("Review Publication Date: 2019-04-15")
       end
     end
