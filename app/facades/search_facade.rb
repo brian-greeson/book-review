31ae0486d9d5d8b111071f7bd06f715ab72ecd4b
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-05-29 10:32:57 -0600

    Update search facade with Service calls

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,4 +1,8 @@
 class SearchFacade < BaseFacade
-  
+  def initialize(search_params)
+    book_json = OpenLibraryService.new.search_by_title(search_params[:title])
+    review_json = NytBookService.new.search_by_title(search_params[:title])
+    @book = Book.new(title: searchparams[:title],)
+  end
 
 end
